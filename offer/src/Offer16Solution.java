2a7433052806ae496faf30e09d2bcb8f98d16036
huruidong <huruidong@meituan.com>
2020-09-07 09:08:38 +0800

    剑指offer16 20200907 实现数字的正数幂计算，二进制格式表示幂次，简化问题为递归问题

--- offer/src/Offer16Solution.java
+++ offer/src/Offer16Solution.java
@@ -21,4 +21,31 @@
  * n是 32 位有符号整数，其数值范围是[−2^31,2^31− 1] 。
  */
 public class Offer16Solution {
+
+    public static void main(String[] args) {
+        //基础验证
+        double result = new Offer16Solution().myPow(2.000,-2147483648);
+        System.out.println(result);
+    }
+
+    public double myPow(double x, int n) {
+        if (0 == x) return 0;
+        //n为负数是获得相反数超限，所以先以long型保存
+        long b = n;
+        if (b < 0) {
+            x = 1 / x;
+            b = -b;
+        }
+        double result = 1;
+        //x^n=x^(1*b1+2*b2+4*b3+...+2^(m-1)*bm)依次获得二进制位是0还是1
+        //x^2=x*x;x^4=x^2*x^2
+        //可以从二进制角度，或者二分法角度理解，二进制角度更加直接
+        while (b != 0) {
+            long bi = b & 1;
+            result *= 1 == bi ? x : 1;
+            x *= x;
+            b >>>= 1;
+        }
+        return result;
+    }
 }
