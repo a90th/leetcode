471529fa65b8840790d31d490ee68027ff79b8b7
huruidong <huruidong@meituan.com>
2020-09-10 10:58:14 +0800

    剑指Offer28-29 20200910 二叉树树的镜像验证和数组的顺时针打印

--- offer/src/Offer28Solution.java
+++ offer/src/Offer28Solution.java
@@ -33,10 +33,25 @@ import java.util.*;
  */
 public class Offer28Solution {
     /**
-     * 方法1：广度优先遍历
+     * 方法1：分治法
+     * 重点：搞明白"对称是双目运算，不是单目运算"，递推的逻辑是"判断root是左右对称的，需要root.left.value=root.right.value，
+     * 并且root.left.left与root.right.right对称，root.left.right与root.right.left对称"
+     *
+     * 不能使用广度优先遍历，因为存在一种遍历结果对应多种属结构的情况
      *
      * @param root
      * @return
      */
     public boolean isSymmetric(TreeNode root) {
+        if (null == root) return true;
+        return isSymmetric(root.left, root.right);
     }
+
+    public boolean isSymmetric(TreeNode A, TreeNode B) {
+        if (null != A && null != B) {
+            return A.val == B.val && isSymmetric(A.left, B.right) && isSymmetric(A.right, B.left);
+        } else {
+            return null == A && null == B;
+        }
+    }
+}
